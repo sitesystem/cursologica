e5724ee11f266ec51b6611b09e4409d2ef018f99
sitesystem <l_aloaxa05@hotmail.com>
2022-05-02 19:49:31 -0500

    Primera actualización

---
+++ PythonCurso/HolaMundo.py
@@ -0,0 +1,10 @@
+# COMENTARIO: Ejercicio de un Saludo
+# Los comentarios sirven para documentar el código o programa
+# y los omite o no los toma en cuenta python
+
+# IMPRIMIR EN PANTALLA O MOSTRAR LA SALIDA EN PANTALLA
+print("HOLA MUNDO")
+print('Eduardo González Bernal');
+print(5+5)
+print(10-2)
+print("CURSO DE LÓGICA")
